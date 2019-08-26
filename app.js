7bd6a9edf2ccc3406c9a9457a2fece072de35a55
TJ Jackson <elitefourfresh@gmail.com>
2019-08-26 01:09:40 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+app.get("/api/all", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send({msg: "Hello, World!"});
+});
+
+// RUN THE SERVER
+app.listen(3000, (e) => {
+    if(e) throw e;
+
+    console.log("Server has started on port 3000...");
+});
